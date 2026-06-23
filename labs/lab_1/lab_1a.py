530a403afd2fa9055586003be1480148c935d865
theabilab <abigailpucylowski@gmail.com>
2026-06-22 21:33:11 -0400

    Update lab_1a.py
    Added my introduction from the beginning of the class.

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -9,7 +9,7 @@ def main():
     print("Hello World!")
 
     name = "Abigail Pucylowski" # TODO: Insert your name between the double quotes
-
+    print("Hi! My name is Abi and I am a rising junior in Rockledge, Florida. I am on a First Robotics team working with both the mechanical and software sides. I work on Arduino and CAD projects at home in my spare time. I have done some summer programs in data science and artificial intelligence. However, I have really enjoyed exploring the hardware side more. I am hoping I can build technology that will help children with autism.")
     print(f"{name}, Welcome to the CSS course!")
 
 if __name__ == "__main__":
